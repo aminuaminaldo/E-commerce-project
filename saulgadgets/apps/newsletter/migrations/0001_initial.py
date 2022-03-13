614dd401edae24ac4339db466f6eaa4ba19de303
aminuaminaldo <aminuashraf198@gmail.com>
2022-03-13 16:28:59 +0300

    database change

--- saulgadgets/apps/newsletter/migrations/0001_initial.py
+++ saulgadgets/apps/newsletter/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=255)),
-                ('date_addedd', models.DateTimeField(auto_now_add=True)),
+                ('date_added', models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
